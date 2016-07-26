eb1806c6b09f641e89fcac693037709b916cc64d
amijan920 <amielreveche@yahoo.com.ph>
2016-07-26 10:26:56 -0700

    Added sample custom tag

---
+++ ApsarasTrips/AsparasTrips/src/main/java/com/apsaras/tags/SampleTag.java
@@ -0,0 +1,16 @@
+package com.apsaras.tags;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class SampleTag extends SimpleTagSupport{
+	
+	@Override
+	public void doTag() throws JspException, IOException {
+		JspWriter out = getJspContext().getOut();
+		out.println("Testing custom tag");
+	}
+}
